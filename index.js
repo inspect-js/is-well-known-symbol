c1f8dd3bdb2f19a8c136e183dd32c52711f00039
Jordan Harband <ljharb@gmail.com>
2024-12-18 23:08:56 -0800

    [Refactor] use 1 iteration instead of 2; drop flatmap dep

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 var hasSymbols = require('has-symbols')();
 var isSymbol = require('is-symbol');
-var flatMap = require('array.prototype.flatmap');
 var ownKeys = require('reflect.ownkeys');
 var callBound = require('call-bound');
 
@@ -11,19 +10,17 @@ var result;
 
 if (hasSymbols) {
 	var symbolValueOf = callBound('Symbol.prototype.valueOf');
-	var wellKnownSymbols = hasSymbols && flatMap(
-		ownKeys(Symbol),
-		function (k) {
-			var v = Symbol[k];
-			// eslint-disable-next-line no-extra-parens
-			return typeof v === 'symbol' ? /** @type {[import('.').WellKnownSymbol]} */ (/** @type {unknown} */ (v)) : [];
-		}
-	);
+	var symKeys = ownKeys(Symbol);
 
 	/** @type {{ __proto__: null } & { [k in import('.').WellKnownSymbol]?: true }} */
 	var map = { __proto__: null };
-	for (var i = 0; i < wellKnownSymbols.length; i += 1) {
-		map[wellKnownSymbols[i]] = true;
+
+	for (var i = 0; i < symKeys.length; i += 1) {
+		var v = Symbol[symKeys[i]];
+		if (typeof v === 'symbol') {
+			// eslint-disable-next-line no-extra-parens
+			map[/** @type {import('.').WellKnownSymbol} */ (/** @type {unknown} */ (v))] = true;
+		}
 	}
 
 	result = /** @type {import('.')} */ function isWellKnownSymbol(sym) {
